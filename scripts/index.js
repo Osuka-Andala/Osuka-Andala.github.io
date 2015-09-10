74119929bb05ed09a8e09ff2c82ee5699b076530
Osuka-Andala <teresa.andala@gmail.com>
2015-09-11 00:29:46 +0300

    Moved images out of main folder and into images folder

--- scripts/index.js
+++ scripts/index.js
@@ -6,4 +6,12 @@ $( document ).ready(function(){
 $(document).ready(function(){
      $('.parallax').parallax();
    });
-       
+
+   var options = [
+       {selector: '#staggered-test', offset: 50, callback: 'Materialize.toast("This is our ScrollFire Demo!", 1500 )' },
+       {selector: '#staggered-test', offset: 205, callback: 'Materialize.toast("Please continue scrolling!", 1500 )' },
+       {selector: '#staggered-test', offset: 400, callback: 'Materialize.showStaggeredList("#staggered-test")' },
+       {selector: '#image-test', offset: 500, callback: 'Materialize.fadeInImage("#image-test")' }
+     ];
+     Materialize.scrollFire(options);
+         
